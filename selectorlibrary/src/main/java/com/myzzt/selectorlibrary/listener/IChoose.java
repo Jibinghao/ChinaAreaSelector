aa1bcdfaeac97bd4aa0c45f64618003ad980164f
jibinghao <769499597@qq.com>
2018-02-27 15:40:56 +0800

    修改IChoose备注

--- selectorlibrary/src/main/java/com/myzzt/selectorlibrary/listener/IChoose.java
+++ selectorlibrary/src/main/java/com/myzzt/selectorlibrary/listener/IChoose.java
@@ -5,6 +5,12 @@ package com.myzzt.selectorlibrary.listener;
  */
 
 public interface IChoose {
+    /**
+     * 由于被屏蔽了，对应以下的字段
+     * 省id,市id,区id,省名字，市名字，区名字
+     * String province_id, String city_id, String district_id, String province_title, String city_name, String district_name
+     */
+
     void onChoose(String province_id, String city_id, String district_id, String province_title, String city_name, String district_name);
 
 }
